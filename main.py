37ec20c1972c5064ba6847ca8ffc316f8e415c9b
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-04-30 13:06:53 +0700

    ready 1
    без красоты

--- main.py
+++ main.py
@@ -3,25 +3,39 @@ from tkinter import messagebox
 
 window = tk.Tk()
 window.title('Крестики-нолики')
-window.geometry('270x300')
+window.geometry('318x360')
 
 current_player = 'X'
 buttons = [] #
 
 def check_winer():
-    pass
+    for i in range(3):
+        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] != '':
+            return True
+        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] != '':
+            return True
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != '':
+        return True
+    if buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != '':
+        return True
+    return False
 
-def on_click(r, c):
-    pass
-
+def on_click(row, col):
+    global current_player
+    if buttons[row][col]['text'] != '':
+        return
+    buttons[row][col]['text'] = current_player
+    if check_winer():
+        messagebox.showinfo('Игра окончена', f'Игрок {current_player} победил')
+    current_player = 'O' if current_player == 'X' else 'X'
 
 for i in range(3):
     row = [] #
     for j in range(3):
-        btn = tk.Button(window, text='', font=('Arial', 20), width=5, height=4, command=lambda r=i, c=j: on_click(r, c))
+        btn = tk.Button(window, text='', font=('Arial', 20), width=6, height=3, command=lambda r=i, c=j: on_click(r, c))
         btn.grid(row=i, column=j)
         row.append(btn)
-        buttons.append(row)
+    buttons.append(row)
 
 
 
