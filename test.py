ff77cfe6989a5bc83afd8a57ad831ec65467fc10
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-04-30 16:20:07 +0700

    text
    текст в верхней части фрейма

--- test.py
+++ test.py
@@ -12,6 +12,14 @@ window.grid_columnconfigure(0, weight=1)
 frame = tk.Frame(window, bg='AntiqueWhite3', width=318, height=500)  # создаем фрэйм
 frame.grid(row=0, column=0, sticky='nsew')  # Используем grid для центрирования
 
+# Добавляем метку
+text1 = tk.Label(frame, text='Выбирите: Х или О', font=('Arial', 16), bg='AntiqueWhite3')
+text1.grid(row=0, column=0, columnspan=3, pady=10)  # Размещаем метку в верхней части фрейма
+
+# Добавляем текстовое поле ввода
+task_entry = tk.Entry(frame, font=('Arial', 12))
+task_entry.grid(row=1, column=0, columnspan=3, pady=5)  # Размещаем текстовое поле под меткой
+
 current_player = 'X'  # устанавливаем текущего игрока
 buttons = []  # список для кнопок
 
@@ -25,8 +33,6 @@ def check_winer():
         return True # если совпала возвращаем выигрышь
     if buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != '': # вторая диагональ
         return True # если совпала возвращаем выигрышь
-    #clear() # не было выигрышей очищаем
-    print('чего тут?')
     return False  # не было выигрышей
 
 def clear():
@@ -43,25 +49,22 @@ def on_click(row, col):  #  нажатие кнопки мыши
     global current_player
     if buttons[row][col]['text'] != '':  # если на кнопке пусто
         return  # обратно к вызывающему
-    buttons[row][col]['text'] = current_player  # если символ на кгопке совпал с текущим игороком
+    buttons[row][col]['text'] = current_player  # если символ на кнопке совпал с текущим игроком
     if check_winer():  # проверяем была ли победа,  если да то:
         messagebox.showinfo('Игра окончена', f'Игрок {current_player} победил')
     else: # победы не было
-        print(f'cодержимое стека2: {buttons[row][col]['text']}')
         game_over += 1
-        print(f'game_over = {game_over}')
         if game_over == 9:  # если было 9 нажатий игра заново, - ничья
             clear()
     current_player = 'O' if current_player == 'X' else 'X' # меняются игроки
 
 for i in range(3):  # строим поле пустых кнопок
-    row = [] # список строк из копок
+    row = [] # список строк из кнопок
     for j in range(3):
         btn = tk.Button(frame, text='', font=('Arial', 20), width=6, height=3, command=lambda r=i, c=j: on_click(r, c))
-        btn.grid(row=i, column=j)
-        row.append(btn) # добавляем кнопку с строку кнопок
-        # print(f'cодержимое стека: {btn['text']}')
+        btn.grid(row=i + 2, column=j)  # Увеличиваем индекс строки на 2, чтобы учесть метку и текстовое поле
+        row.append(btn) # добавляем кнопку в строку кнопок
 
-    buttons.append(row) # добавляем строку кнопок а список кнопок, - столбцы
+    buttons.append(row) # добавляем строку кнопок в список кнопок
 
 window.mainloop()
